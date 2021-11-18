1c24db2a60724c5126a7eea4eb485bf95fe411bb
Alejandro <alejandrolealastorga@gmail.com>
2021-11-18 14:39:00 +0100

    Rama 1 git

---
+++ Git2.java
@@ -0,0 +1,21 @@
+package Funciones;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int [] array = new int [20];
+	
+	for (int i = 0; i < array.length; i++) {
+		array[i] = (int)(Math.random()*10);
+		System.out.print(array[i]+ ",");
+	}
+			
+	
+	
+	
+	
+	}
+
+}
