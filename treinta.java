5b0112f4ff1526191bbe349030ab6d4e1d6fb58a
Alejandro <alejandrolealastorga@gmail.com>
2021-11-25 14:07:07 +0100

    Fusion1

---
+++ treinta.java
@@ -0,0 +1,22 @@
+package Funciones;
+
+public class treinta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int [] array = new int [30];
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random()*10);
+			System.out.print(array[i]+ ",");
+		}
+
+		
+		
+		
+	
+	}
+
+}
